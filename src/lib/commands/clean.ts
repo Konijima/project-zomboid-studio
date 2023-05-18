a821afee9b29869a2bcc706bd228d195a80057f5
Konijima <mathieugermain1991@gmail.com>
2023-05-18 18:17:12 -0400

    Update clean.ts

--- src/lib/commands/clean.ts
+++ src/lib/commands/clean.ts
@@ -1,5 +1,8 @@
+import { join } from "path";
+import { existsSync, rmSync } from "fs";
 import { addHelp } from "../help";
-import { readProjectConfig } from "../helper";
+import { getOutDir, readProjectConfig } from "../helper";
+import { log } from "../logger";
 
 addHelp('clean', `Clean your output directory from the current built project.
 
@@ -7,10 +10,22 @@ addHelp('clean', `Clean your output directory from the current built project.
         pzstudio clean - Clean your output directory from the current built project.`);
     
 export function cleanCmd() {
-    throw new Error('Not implemented yet!');
-    
+    const projectConfig = readProjectConfig();
+
     // Check if we are in a project directory
-    if (!readProjectConfig()) {
+    if (!projectConfig) {
         throw new Error('You must execute this command within a project directory!');
     }
+
+    const startTime = performance.now();
+
+    // Clean the output directory
+    const outPath = join(getOutDir(), projectConfig.title);
+    if (existsSync(outPath)) {
+        log(`Cleaning output directory at '${outPath}'...`);
+        rmSync(outPath, { recursive: true });
+    }
+
+    const endTime = performance.now();
+    log(`Clean complete in ${((endTime - startTime) / 1000).toFixed(2)}s!`);
 }
