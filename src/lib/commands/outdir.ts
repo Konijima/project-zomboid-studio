178968b2e49816826bcddd9d8d89666b3a1f5505
Konijima <mathieugermain1991@gmail.com>
2023-05-18 13:30:25 -0400

    Update outdir.ts

--- src/lib/commands/outdir.ts
+++ src/lib/commands/outdir.ts
@@ -1,5 +1,9 @@
 import { expect } from "../expect";
 import { addHelp } from "../help";
+import { resolve } from "path";
+import { existsSync, readFileSync, writeFileSync } from "fs";
+import { getStoreDir } from "../helper";
+import { log } from "../logger";
 
 addHelp('outdir', `Change the output directory of your project.
 
@@ -8,4 +12,26 @@ addHelp('outdir', `Change the output directory of your project.
 
 export function outdirCmd(newOutDir: string) {
     expect('param [newOutDir]', newOutDir, 'string');
+
+    let storePath = getStoreDir();
+    
+    // resolve the path
+    newOutDir = resolve(newOutDir);
+
+    // check if the new path exists
+    if (!existsSync(newOutDir)) {
+        throw new Error(`The output directory "${newOutDir}" does not exist.`);
+    }
+
+    // check if the new path is the same as the old one
+    if (existsSync(storePath)) {
+        const oldPath = readFileSync(storePath, 'utf8');
+        if (oldPath === newOutDir) {
+            throw new Error('The output directory is already set to this value.');
+        }
+    }
+
+    // write the new path
+    writeFileSync(storePath, newOutDir);
+    log(`The output directory has been changed to "${newOutDir}".`);
 }
