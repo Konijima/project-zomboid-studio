94af44485c4dc525cfed78e0e23bcc8ca3837a18
Konijima <mathieugermain1991@gmail.com>
2023-05-18 15:23:59 -0400

    Create languages.ts

---
+++ src/lib/languages.ts
@@ -0,0 +1,139 @@
+const Languages = {
+    AR: {
+        code: 'AR',
+        name: 'Espanol (AR)',
+        charset: 'windows-1252',
+    },
+    CA: {
+        code: 'CA',
+        name: 'Catalan',
+        charset: 'iso-8859-15',
+    },
+    CH: {
+        code: 'CH',
+        name: 'Traditional Chinese',
+        charset: 'utf-8',
+    },
+    CN: {
+        code: 'CN',
+        name: 'Simplified Chinese',
+        charset: 'utf-8',
+    },
+    CS: {
+        code: 'CS',
+        name: 'Czech',
+        charset: 'windows-1250',
+    },
+    DA: {
+        code: 'DA',
+        name: 'Danish',
+        charset: 'windows-1252',
+    },
+    DE: {
+        code: 'DE',
+        name: 'Deutsch',
+        charset: 'windows-1252',
+    },
+    EN: {
+        code: 'EN',
+        name: 'English',
+        charset: 'utf-8',
+    },
+    ES: {
+        code: 'ES',
+        name: 'Espanol (ES)',
+        charset: 'windows-1252',
+    },
+    FI: {
+        code: 'FI',
+        name: 'Finnish',
+        charset: 'windows-1252',
+    },
+    FR: {
+        code: 'FR',
+        name: 'Francais',
+        charset: 'windows-1252',
+    },
+    HU: {
+        code: 'HU',
+        name: 'Hungarian',
+        charset: 'windows-1250',
+    },
+    ID: {
+        code: 'ID',
+        name: 'Indonesia',
+        charset: 'utf-8',
+    },
+    IT: {
+        code: 'IT',
+        name: 'Italiano',
+        charset: 'windows-1252',
+    },
+    JP: {
+        code: 'JP',
+        name: 'Japanese',
+        charset: 'utf-8',
+    },
+    KO: {
+        code: 'KO',
+        name: 'Korean',
+        charset: 'utf-16',
+    },
+    NL: {
+        code: 'NL',
+        name: 'Nederlands',
+        charset: 'windows-1252',
+    },
+    NO: {
+        code: 'NO',
+        name: 'Norsk',
+        charset: 'windows-1252',
+    },
+    PH: {
+        code: 'PH',
+        name: 'Tagalog',
+        charset: 'utf-8',
+    },
+    PL: {
+        code: 'PL',
+        name: 'Polish',
+        charset: 'windows-1250',
+    },
+    PT: {
+        code: 'PT',
+        name: 'Portuguese',
+        charset: 'windows-1252',
+    },
+    PTBR: {
+        code: 'PTBR',
+        name: 'Brazilian Portuguese',
+        charset: 'windows-1252',
+    },
+    RO: {
+        code: 'RO',
+        name: 'Romanian',
+        charset: 'utf-8',
+    },
+    RU: {
+        code: 'RU',
+        name: 'Russian',
+        charset: 'windows-1251',
+    },
+    TH: {
+        code: 'TH',
+        name: 'Thai',
+        charset: 'utf-8',
+    },
+    TR: {
+        code: 'TR',
+        name: 'Turkish',
+        charset: 'Cp1254',
+    },
+    UA: {
+        code: 'UA',
+        name: 'Ukrainian',
+        charset: 'windows-1251',
+    },
+}
+
+export { Languages }
