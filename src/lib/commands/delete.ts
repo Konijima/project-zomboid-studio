14979444d5b8bd016fa9a84801daec70432f1da7
Konijima <mathieugermain1991@gmail.com>
2023-05-18 14:11:50 -0400

    Update delete.ts

--- src/lib/commands/delete.ts
+++ src/lib/commands/delete.ts
@@ -1,5 +1,9 @@
+import { join } from "path";
+import { existsSync, rmSync } from "fs";
 import { expect } from "../expect";
 import { addHelp } from "../help";
+import { log } from "../logger";
+import { projectDir, readProjectConfig, updateProjectConfig } from "../helper";
 
 addHelp('delete', `Delete a mod from your project.
 
@@ -7,5 +11,38 @@ addHelp('delete', `Delete a mod from your project.
         pzstudio delete <modId> - Delete a mod from your project.`);
 
 export function deleteCmd(modId: string) {
+    // Check if we are in a project directory
+    if (!readProjectConfig()) {
+        throw new Error('You must execute this command within a project directory!');
+    }
+
+    // Validate params
     expect('param [modId]', modId, 'string');
+
+    const projectPath = projectDir();
+    const projectConfigPath = join(projectPath, 'project.json');
+    const projectConfig = readProjectConfig(projectConfigPath);
+
+    // Check if mod already exists
+    if (!projectConfig.mods[modId]) {
+        throw new Error(`Mod ${modId} does not exist!`);
+    }
+
+    // Delete mod
+    if (existsSync(join(projectPath, 'lua', modId))) {
+        rmSync(join(projectPath, 'lua', modId), { force: true, recursive: true });
+    }
+    if (existsSync(join(projectPath, 'mods', modId))) {
+        rmSync(join(projectPath, 'mods', modId), { force: true, recursive: true });
+    }
+    if (existsSync(join(projectPath, 'translations', modId))) {
+        rmSync(join(projectPath, 'translations', modId), { force: true, recursive: true });
+    }
+    
+    // Save project config
+    delete projectConfig.mods[modId];
+    updateProjectConfig(projectConfigPath, projectConfig);
+
+    // Done
+    log(`Deleted mod '${modId}'`);
 }
